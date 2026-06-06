bbb5e15cf40b68bd29dbc11e587692545639335c
Aaron Fleming <37069089+a-fleming@users.noreply.github.com>
2026-06-06 00:49:45 -0700

    feat: load GitHub credentials from .env

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+
+def load_credentials() -> tuple[str, str]:
+    load_dotenv()
+    github_username = os.environ.get("GITHUB_USERNAME")
+    if not github_username:
+        raise RuntimeError("GITHUB_USERNAME environment variable not set")
+    github_password = os.environ.get("GITHUB_PASSWORD")
+    if not github_password:
+        raise RuntimeError("GITHUB_PASSWORD environment variable not set")
+    return github_username, github_password
+
 def main():
-    print("Hello from advent!")
+    github_username, github_password = load_credentials()
+    print(f"Username: {github_username}")
 
 
 if __name__ == "__main__":
